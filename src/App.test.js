c6aa0ea6616e061499fe00d936a8a75ef15728b4
unknown <sujanbhushal2014@gmail.com>
2024-11-28 18:16:23 +0545

    Test adding and displaying a single todo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('if the heading is correct', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/Todo's/i);
+test('typing into input and displaying todo', () => {
+  render(<App />);
+
+  const inputElement = screen.getByPlaceholderText(/Add a new todo .../i);
+  const buttonElement = screen.getByRole('button', { name: /Add Todo/i });
+
+  fireEvent.change(inputElement, { target: { value: 'Send a mail to Dad' } });
+  fireEvent.click(buttonElement);
+
+  const todoElement = screen.getByText(/Send a mail to Dad/i);
+  expect(todoElement).toBeInTheDocument();
 });
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -16,7 +16,7 @@ const AddTodo = ({ todos, addTodo }) => {
 
     return (
         <div className="add-todo__container">
-            <input type="text" className="add-todo__input" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <input type="text" className="add-todo__input" value={todo} placeholder="Add a new todo ..." onChange={(e) => setTodo(e.target.value)} />
             <button className="add-todo__button" onClick={handleSubmit}>Add Todo</button>
         </div>
     )
